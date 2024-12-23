eaa00fc02fe65b30bc4f57ca4bf384bc6ae3b203
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-12-23 10:20:51 +0100

    Wrap React app with the Provider component

--- user-front/src/index.js
+++ user-front/src/index.js
@@ -5,17 +5,18 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ApolloProvider } from "@apollo/client";
 import client from "./configuration/apollo-client";
+import store from "./redux/store/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
+    <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
+    </Provider>
   </ApolloProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
