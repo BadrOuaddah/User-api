acd7b02c66d4dcbbc3ddb93863b5e2d39c8fd393
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-12-31 16:47:34 +0100

    Add data.DELETE_USER in deleteUser constant

--- user-front/src/redux/slices/userSlice.js
+++ user-front/src/redux/slices/userSlice.js
@@ -1,6 +1,11 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import client from "../../configuration/apollo-client";
-import { GET_ALL_USERS, CREATE_USER, UPDATE_USER, DELETE_USER } from "../../graphql/requires";
+import {
+  GET_ALL_USERS,
+  CREATE_USER,
+  UPDATE_USER,
+  DELETE_USER
+} from "../../graphql/requires";
 
 export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
   const { data } = await client.query({ query: GET_ALL_USERS });
@@ -10,33 +15,39 @@ export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
 export const addUser = createAsyncThunk("users/addUser", async (userInput) => {
   const { data } = await client.mutate({
     mutation: CREATE_USER,
-    variables: { user: userInput },
+    variables: { user: userInput }
   });
   return data.createUser;
 });
 
-export const updateUser = createAsyncThunk("users/updateUser", async ({ userId, userInput }) => {
-  const { data } = await client.mutate({
-    mutation: UPDATE_USER,
-    variables: { userId, user: userInput },
-  });
-  return data.updateUser;
-});
+export const updateUser = createAsyncThunk(
+  "users/updateUser",
+  async ({ userId, userInput }) => {
+    const { data } = await client.mutate({
+      mutation: UPDATE_USER,
+      variables: { userId, user: userInput }
+    });
+    return data.updateUser;
+  }
+);
 
-export const deleteUser = createAsyncThunk("users/deleteUser", async (userId) => {
-  const { data } = await client.mutate({
-    mutation: DELETE_USER,
-    variables: { id: userId },
-  });
-  return userId;
-});
+export const deleteUser = createAsyncThunk(
+  "users/deleteUser",
+  async (userId) => {
+    const { data } = await client.mutate({
+      mutation: DELETE_USER,
+      variables: { id: userId }
+    });
+    return data.DELETE_USER;
+  }
+);
 
 const userSlice = createSlice({
   name: "users",
   initialState: {
     userList: [],
     loading: false,
-    error: null,
+    error: null
   },
   reducers: {},
   extraReducers: (builder) => {
@@ -58,16 +69,20 @@ const userSlice = createSlice({
     });
 
     builder.addCase(updateUser.fulfilled, (state, action) => {
-      const index = state.userList.findIndex((user) => user.id === action.payload.id);
+      const index = state.userList.findIndex(
+        (user) => user.id === action.payload.id
+      );
       if (index !== -1) {
         state.userList[index] = action.payload;
       }
     });
 
     builder.addCase(deleteUser.fulfilled, (state, action) => {
-      state.userList = state.userList.filter((user) => user.id !== action.payload);
+      state.userList = state.userList.filter(
+        (user) => user.id !== action.payload
+      );
     });
-  },
+  }
 });
 
 export default userSlice.reducer;
